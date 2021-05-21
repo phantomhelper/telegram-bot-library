c6f737f542ba07cb6b6a54f0d0b85636957add2d
phantomhelper <dankaonpc@gmail.com>
2021-05-21 23:14:03 +0300

    +

--- main/handlers/users/admin_panel.py
+++ main/handlers/users/admin_panel.py
@@ -14,33 +14,7 @@ with open('config.json', 'r', encoding="utf8") as f:
     config = json.load(f)
 buff_id = config['number']
 
-class Test(StatesGroup):
-    Q1=State()
-    Q2=State()
-
-@dp.message_handler(Command("test"), state=None)
-async def enter_test(message: Message):
-    await message.answer("Как вас зовут?")
-    await Test.Q1.set()
-
-@dp.message_handler(state=Test.Q1)
-async def answer_q1(message: Message, state: FSMContext):
-    answer = message.text
-    await state.update_data(answer1=answer)
-    await message.answer("Возраст:")
-    await Test.next()
-
-@dp.message_handler(state=Test.Q2)
-async def answer_q2(message: Message, state: FSMContext):
-    answer2=message.text
-    data = await state.get_data()
-    answer1=data.get("answer1")
-    answer2=message.text
-    await message.answer(f"+OK\n{answer1}\n{answer2}")
-    await state.finish()
-
-
-"""class passage(StatesGroup):
+class passage(StatesGroup):
     title = State()
     brief = State()
     telegraph_url = State()
@@ -48,56 +22,68 @@ async def answer_q2(message: Message, state: FSMContext):
     photo_path = State()
     genre = State()
 
-@dp.message_handler(commands=['add'])
-async def add_title(message: Message, state: FSMContext):
+@dp.message_handler(Command("add"), state=None)
+async def add_psg(message: Message):
     if message.chat.id in admins:
-        await message.answer(text='Заголовок:')
-        await passage.title.set()
-        async with state.proxy() as data:
-            data['name'] = message.text
-
-        await passage.next()
-        print(passage.title)
+        await message.answer("Заголовок:")
+        await passage.first()
 
 @dp.message_handler(state=passage.title)
-async def add_brief(message: Message, state: FSMContext):
-    await message.answer(text='Краткий рассказ:')
-    await passage.brief.set()
-    print(passage.title)
+async def answer_q1(message: Message, state: FSMContext):
+    answer = message.text # ЗАГОЛОВОК
+    await state.update_data(answer1=answer)
+    await message.answer("Краткий рассказ:")
+    await passage.next()
 
 @dp.message_handler(state=passage.brief)
-async def add_brief(message: Message, state: FSMContext):
-    await message.answer(text='Ссылка:')
-    await passage.telegraph_url.set()
+async def answer_q2(message: Message, state: FSMContext):
+    answer = message.text # КРАТКИЙ РАССКАЗ
+    await state.update_data(answer2=answer)
+    await message.answer("Ссылка:")
+    await passage.next()
 
-    @dp.message_handler(state=passage.telegraph_url)
-    async def add_brief(message: Message, state: FSMContext):
-        await message.answer(text='Фото:')
-        await passage.photo_path.set()
-
-@dp.message_handler(state=passage.photo_path)
-async def add_brief(message: Message, state: FSMContext):
-    await message.answer(text='Аудио (приоритет):')
-    await passage.audio_path.set()
+@dp.message_handler(state=passage.telegraph_url)
+async def answer_q3(message: Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(answer3=answer)
+    await message.answer("Аудио (приоритет):")
+    await passage.next()
 
 @dp.message_handler(state=passage.audio_path)
-async def add_brief(message: Message, state: FSMContext):
-    await message.answer(text='Жанр:')
-    await passage.genre.set()
+async def answer_q4(message: Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(answer4=answer)
+    await message.answer("Фото:")
+    await passage.next()
+
+@dp.message_handler(state=passage.photo_path)
+async def answer_q5(message: Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(answer5=answer)
+    await message.answer("Жанр:")
+    await passage.next()
+
+
+@dp.message_handler(state=passage.genre)
+async def answer_q6(message: Message, state: FSMContext):
+    answer6=message.text
+    data = await state.get_data()
+    answer1=data.get("answer1")
+    answer6=message.text
+
     data = {
         "id" : buff_id+1,
-        "text" : str(passage.title),
-        "brief" : str(passage.brief),
-        "telegraph_url" : str(passage.telegraph_url),
-        "audio_path" : str(passage.audio_path),
-        "photo_path" : str(passage.photo_path),
-        "genre" : str(passage.genre),
+        "text" : str(data.get("answer1")),
+        "brief" : str(data.get("answer2")),
+        "telegraph_url" : str(data.get("answer3")),
+        "audio_path" : str(data.get("answer4")),
+        "photo_path" : str(data.get("answer5")),
+        "genre" : str(answer6),
         "rating" : 0
     }
 
+    await state.finish()
     db_passagese.insert_one(data)
-
     with open("config.json", "w") as write_file:
         json.dump(config, write_file, indent=4)
-    await message.answer(text="+OK")
-"""
+    await message.answer(text="✅")
--- main/handlers/users/daily_msg.py
+++ main/handlers/users/daily_msg.py
@@ -8,15 +8,15 @@ from threading import Thread
 from keyboards.default import menu
 from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp, bot, db_users, db_passagese, db_messages, db_users_shelf
-from aiogram.types import CallbackQuery, Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import CallbackQuery, Message, InlineKeyboardMarkup, InlineKeyboardButton, MediaGroup
 
 from handlers.users.start import _random_passage
 
 with open('config.json', 'r', encoding="utf8") as f:
     config = json.load(f)
 
-time_day = '09:00'
-#time_day = str(time.strftime("%H:%M", time.localtime())) # NOTE: ONLY FOR TESTS; TURN OFF
+#time_day = '09:00'
+time_day = str(time.strftime("%H:%M", time.localtime())) # NOTE: ONLY FOR TESTS; TURN OFF
 time_night = '20:00'
 
 async def daily_messages():
@@ -30,7 +30,11 @@ async def daily_messages():
             i = 1
             while i <= config['users']:
                 user = db_users.find_one({"id": i})
-                text = f"<b>{text}</b>"
+                text = f"<i>{text}</i>"
+                """if buff[0]['photo_path'] != None:
+                    media = types.MediaGroup()
+                    media.attach_photo(types.InputFile('123.jpg'), 'text', reply_markup=buff[1])
+                    await message.reply_media_group(media=media)"""
                 message_id = await bot.send_message(chat_id=user['tid'], text=text, reply_markup=buff[1])
                 data = {
                 "mid" : message_id['message_id'],
@@ -39,10 +43,11 @@ async def daily_messages():
                 }
                 db_messages.insert_one(data)
                 time.sleep(5)
+                i+=1
         else:
             time.sleep(60)
 
 
 
-"""loop = asyncio.get_event_loop()
-loop.run_until_complete(daily_messages())"""
+loop = asyncio.get_event_loop()
+loop.run_until_complete(daily_messages())
